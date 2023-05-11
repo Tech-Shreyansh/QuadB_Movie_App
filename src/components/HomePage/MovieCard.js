59ef86433ac7d1c90077b2947e007ad29f7e4c9d
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2023-05-11 11:02:47 +0530

    completed home page

--- src/components/HomePage/MovieCard.js
+++ src/components/HomePage/MovieCard.js
@@ -11,11 +11,51 @@ function MovieCard(item) {
     rating = item.item.show.rating.average
     
     return(
+        <div className="card">
+        <p className="cardBlock">{item.item.show.name}</p>
+        <div className="back">
         <div>
-        <p id="cardBlock">{item.item.show.name}</p>
-        <div id="movieCard">
+        <span>Type : </span>
+        <span>{item.item.show.type}</span>
+        </div>
+        <div>
+        <span>Language : </span>
+        <span>{item.item.show.language}</span>
+        </div>
+        <div>
+        <span>Genres : </span>
+        <span>{item.item.show.genres}</span>
+        </div>
+        <div>
+        <span>Status : </span>
+        <span>{item.item.show.status}</span>
+        </div>
+        <div>
+        <span>Runtime : </span>
+        <span>{item.item.show.runtime}</span>
+        </div>
+        <div>
+        <span>Average Runtime : </span>
+        <span>{item.item.show.averageRuntime}</span>
+        </div>
+        <div>
+        <span>Premierd : </span>
+        <span>{item.item.show.premiered}</span>
+        </div>
+        <div>
+        <span>Ended : </span>
+        <span>{item.item.show.ended}</span>
+        </div>
+        <div>
+        <span>Schedule : </span>
+        <span>{item.item.show.schedule.time} </span>on
+        <span> {item.item.show.schedule.days}</span>
+        </div>
+        <div className="book"> Book Now </div>
+        </div>
+        <div className="movieCard">
         <img src={src} alt={item.item.show.name} />
-        <p id="score" > Score: {item.item.score} <br/>Rating : {rating}</p>
+        <p className="score" > Score: {item.item.score} <br/>Rating : {rating}</p>
         </div>
         </div>
     )
